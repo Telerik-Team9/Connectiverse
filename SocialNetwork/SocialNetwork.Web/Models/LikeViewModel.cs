52b184b275242a079985a232e1b63e43df99979d
Ali Marekov <a.marekov@outlook.com>
2020-11-16 16:29:53 +0200

    Add like styles

--- SocialNetwork/SocialNetwork.Web/Models/LikeViewModel.cs
+++ SocialNetwork/SocialNetwork.Web/Models/LikeViewModel.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SocialNetwork.Web.Models
 {
     public class LikeViewModel
     {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public Guid UserId { get; set; }
+        public string UserDisplayName { get; set; }
     }
 }
