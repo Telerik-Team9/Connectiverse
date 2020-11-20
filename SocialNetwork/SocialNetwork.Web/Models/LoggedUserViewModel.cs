31c80765128b740b765f5ea0de0766f5ac5cbda8
Magdalena Nikolova <magi.nikolova@gmail.com>
2020-11-20 16:14:48 +0200

    Modify CreatePost functionality in Profile (NOT FINISHED)

---
+++ SocialNetwork/SocialNetwork.Web/Models/LoggedUserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Web.Models
+{
+    public class LoggedUserViewModel
+    {
+        // TODO: add properties
+
+        public UserViewModel userViewModel { get; set; }
+    }
+}
