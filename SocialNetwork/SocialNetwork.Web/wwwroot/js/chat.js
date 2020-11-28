de5c72426c14581290d374a1308014cab43e5ea5
Magdalena Nikolova <magi.nikolova@gmail.com>
2020-11-28 13:11:04 +0200

    Modify Messenger

--- SocialNetwork/SocialNetwork.Web/wwwroot/js/chat.js
+++ SocialNetwork/SocialNetwork.Web/wwwroot/js/chat.js
@@ -13,29 +13,35 @@ const whenInput = document.getElementById('when');
 const chat = document.getElementById('chat');
 const messagesQueue = [];
 
-//document.getElementById('submitButton').addEventListener('click', () => {
-//    var currentdate = new Date();
-//    when.innerHTML =
-//        (currentdate.getMonth() + 1) + "/"
-//        + currentdate.getDate() + "/"
-//        + currentdate.getFullYear() + " "
-//        + currentdate.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
-//});
-
-//function clearInputField() {
-//    messagesQueue.push(textInput.value);
-//    textInput.value = "";
-//}
-
-//function sendMessage() {
-//    let text = messagesQueue.shift() || "";
-//    if (text.trim() === "") return;    
-//    let when = new Date();
-//    let message = new Message(username, text);
-//    sendMessageToHub(message);
-//}
-
 function addMessageToChat(message) {
+
+    let isCurrentUserMessage = true;
+    let container = document.createElement('div');
+    container.className = isCurrentUserMessage ? "container darker" : "container";
+
+    let sender = document.createElement('p');
+    sender.className = "sender";
+    sender.innerHTML = username;
+    let text = document.createElement('p');
+    text.innerHTML = message;
+
+    let when = document.createElement('span');
+    when.className = isCurrentUserMessage ? "time-left" : "time-right";
+    var currentdate = new Date();
+    when.innerHTML =
+        (currentdate.getMonth() + 1) + "/"
+        + currentdate.getDate() + "/"
+        + currentdate.getFullYear() + " "
+        + currentdate.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+
+    container.appendChild(sender);
+    container.appendChild(text);
+    container.appendChild(when);
+    chat.appendChild(container);
+}
+
+
+/*function addMessageToChat(message) {
 
     let isCurrentUserMessage = true;
     let container = document.createElement('div');
@@ -60,4 +66,4 @@ function addMessageToChat(message) {
     container.appendChild(text);
     container.appendChild(when);
     chat.appendChild(container);
-}
+}*/
