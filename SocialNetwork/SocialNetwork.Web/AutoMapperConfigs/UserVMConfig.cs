6afc60052ae2c7bec8c01a660e92395fe0b1c3ba
Ali Marekov <a.marekov@outlook.com>
2020-12-01 23:33:48 +0200

    Resolve Remove friend issue

--- SocialNetwork/SocialNetwork.Web/AutoMapperConfigs/UserVMConfig.cs
+++ SocialNetwork/SocialNetwork.Web/AutoMapperConfigs/UserVMConfig.cs
@@ -12,7 +12,9 @@ namespace SocialNetwork.Web.AutoMapperConfigs
         {
             this.CreateMap<User, UserDTO>()
                 .ForMember(opt => opt.Posts,
-                           dest => dest.MapFrom(x => x.Posts.Where(p => !p.IsDeleted)))/*.ForMember(opt => opt.Friends, dest => dest.MapFrom(u => u.Friends.Where(f => !f.IsDeleted)))*/
+                           dest => dest.MapFrom(x => x.Posts.Where(p => !p.IsDeleted)))
+                .ForMember(opt => opt.Friends,
+                           dest => dest.MapFrom(u => u.Friends.Where(f => !f.IsDeleted))) // Bug fixed
                 .ReverseMap();
             this.CreateMap<UserDTO, UserViewModel>()
                 .ReverseMap();
